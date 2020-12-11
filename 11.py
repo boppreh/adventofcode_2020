913a1444cbb72a23be74a020e76ce915e1175505
BoppreH <boppreh@gmail.com>
2020-12-11 21:55:16 +0100

    Solve day 11 part 2

--- 11.py
+++ 11.py
@@ -12,14 +12,22 @@ input = open('11.txt')
 matrix = [[char for char in line.strip()] for line in input]
 print(matrix)
 next_matrix = []
-def get_one(row, col):
-    if 0 <= row < len(matrix) and 0 <= col < len(matrix[0]):
-        yield matrix[row][col]
+view_distance = lambda: [1]
+max_neighbors = 4
+def get_neighbors_direction(brow, bcol, drow, dcol):
+    for distance in view_distance():
+        row = brow + drow * distance
+        col = bcol + dcol * distance
+        if 0 <= row < len(matrix) and 0 <= col < len(matrix[0]):
+            if matrix[row][col] != '.':
+                return matrix[row][col]
+        else:
+            return '.'
 def get_neighbors(row, col):
     for drow in (-1, 0, 1):
         for dcol in (-1, 0, 1):
-            if drow != 0 or dcol != 0:
-                yield from get_one(row+drow, col+dcol)
+            if drow == 0 and dcol == 0: continue
+            yield get_neighbors_direction(row, col, drow, dcol)
 def count_occupied_neighbors(row, col):
     return len([pos for pos in get_neighbors(row, col) if pos == '#'])
 def count_all_occupied():
@@ -35,11 +43,23 @@ def step():
             if matrix[row][col] == '.': continue
             if matrix[row][col] == 'L' and count_occupied_neighbors(row, col) == 0:
                 next_matrix[row][col] = '#'
-            elif matrix[row][col] == '#' and count_occupied_neighbors(row, col) >= 4:
+            elif matrix[row][col] == '#' and count_occupied_neighbors(row, col) >= max_neighbors:
                 next_matrix[row][col] = 'L'
     matrix[:] = next_matrix[:]
 
+"""
 print_matrix()
+print(count_all_occupied()) # 2338
+for i in range(100):
+    step()
+    print_matrix()
+    print(count_all_occupied()) # 2338
+"""
+import itertools
+view_distance = lambda: itertools.count(1)
+max_neighbors = 5
+print_matrix()
+print(count_all_occupied()) # 2338
 for i in range(100):
     step()
     print_matrix()
